2085b73d0330514b46a9030346bef0451c4bc1a5
lepremieraxel <axelmarcial151003@gmail.com>
2022-05-07 21:35:36 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const form = document.querySelector('form');
+const liste = document.querySelector('ul');
+const input = document.querySelector('input');
+let toutesLesTaches = [];
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const text = input.value.trim();
+  if(text !== ''){
+    rajouterUneTache(text);
+    input.value = '';
+  }
+})
+
+function rajouterUneTache(text){
+  const todo = {
+    text,
+    id: Date.now()
+  }
+  afficherListe(todo);
+}
+
+function afficherListe(todo){
+  const item = document.createElement('li');
+  item.setAttribute('data-key', todo.id);
+  const input = document.createElement('input');
+  input.setAttribute('type', 'checkbox');
+  input.addEventListener('click', tacheFaite);
+  const txt = document.createElement('span');
+  item.appendChild(input);
+  txt.innerText = todo.text;
+  item.appendChild(txt);
+  const btn = document.createElement('button');
+  btn.addEventListener('click', supprimerTache);
+  const img = document.createElement('img');
+  img.setAttribute('src', 'ressources/fermer.svg');
+  btn.appendChild(img);
+  item.appendChild(btn);
+
+  liste.appendChild(item);
+  toutesLesTaches.push(item);
+}
+
+function tacheFaite(e){
+  e.target.parentNode.classList.toggle('finDeTache');
+}
+
+function supprimerTache(e){
+  toutesLesTaches.forEach(el => {
+    if(e.target.parentNode.getAttribute('data-key') === el.getAttribute('data-key')){
+      el.remove();
+      toutesLesTaches = toutesLesTaches.filter(li => li.dataset.key !== el.dataset.key);
+    }
+  })
+}
